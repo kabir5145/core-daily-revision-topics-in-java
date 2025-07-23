285ef03546c254166475d27a91020a6360d7d904
kabir5145 <kabir10112006@gmail.com>
2025-07-23 22:55:40 +0530

    hashmap question 2 in java

--- com/company/hash.java
+++ com/company/hash.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class hash {
     public static void main(String[] args) {
         //question 1
+        /*
         HashMap<Integer,String > num = new HashMap<>();
         num.put(1,"kabir");
         num.put(2,"raghav");
@@ -14,5 +15,15 @@ public class hash {
         for (Map.Entry x : num.entrySet()){
             System.out.println(x.getKey()+"--> "+x.getValue());
         }
+         */
+
+        //question 2
+        HashMap<Integer,String > siz = new HashMap<Integer,String>();
+        siz.put(1,"hello");
+        siz.put(2,"good");
+        siz.put(3,"bad");
+        siz.put(4,"sad");
+
+            System.out.println(siz.size());
     }
 }
