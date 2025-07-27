a84f27c9710d08c15bf156be2cbe91c4611feae9
kabir5145 <kabir10112006@gmail.com>
2025-07-27 13:14:37 +0530

    array question 2 in java

--- com/company/array.java
+++ com/company/array.java
@@ -5,6 +5,7 @@ import java.util.Arrays; // Import required to use Arrays.toString
 public class array {
     public static void main(String[] args) {
         // Question 1: Write a Java program to sort a numeric array and a string array
+        /*
         int[] number = {1, 5, 7, 3, 4, 9, 10};
 
         System.out.println("The original array is: " + Arrays.toString(number));
@@ -21,6 +22,14 @@ public class array {
         Arrays.sort(word);
 
         System.out.println("The sorted array is :"+Arrays.toString(word));
+         */
+
+        //question 2: sum all values in an array
+        int [] num = {12,34,45};
+        int sum = 0;
+        for(int i :num)
+            sum += i;
+        System.out.println("The sum of an array is : "+sum);
     }
 }
 
