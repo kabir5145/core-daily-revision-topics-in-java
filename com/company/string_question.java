60f947e58219adedeb82d9ff412b6f7897170ae4
kabir5145 <kabir10112006@gmail.com>
2025-07-31 19:35:39 +0530

    string question 2 in java

--- com/company/string_question.java
+++ com/company/string_question.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 public class string_question {
     public static void main(String[] args) {
         // question 1 :reversing a string
+        /*
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         String reversed = new StringBuilder(s).reverse().toString();
         System.out.println("The reverse of this string is : "+reversed);
+         */
+
+        //question 2: get character at index
+        String s = "google company";
+        System.out.println(s);
+        System.out.println(s.charAt(0));
+        System.out.println(s.charAt(9));
     }
 }
