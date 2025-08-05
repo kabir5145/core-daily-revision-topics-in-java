34d0658fca65bfee87a6dff6ff61f0c275579a6b
kabir5145 <kabir10112006@gmail.com>
2025-08-05 23:42:32 +0530

    question 2 of oops   in java

--- com/company/oops.java
+++ com/company/oops.java
@@ -1,5 +1,25 @@
 package com.company;
 
+class question_2{
+    public String name;
+    public int age;
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+/*
 class question_1{
     public String name;
     public int age;
@@ -20,16 +40,23 @@ class question_1{
         this.age = age;
     }
 }
+ */
 
 public class oops {
     public static void main(String[] args) {
+        /*
         question_1 s = new question_1();
         s.setAge(23);
         s.setName("kabir");
         System.out.println("the first name of the person is "+s.getName() +" and age is "+s.getAge());
         s.setName("raghav");
-        System.out.println(s.getName());
+        System.out.println(s.getName);
+         */
 
+        question_2 s = new question_2();
+        s.setAge(3);
+        s.setName("booz");
+        System.out.println(s.getAge()+s.getName());
         }
     }
 
