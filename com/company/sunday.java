1b092adc5832e22303dd13ab702fcbff886b0451
kabir5145 <kabir10112006@gmail.com>
2025-08-17 01:12:26 +0530

    question 2 of Exception Handling

--- com/company/sunday.java
+++ com/company/sunday.java
@@ -4,23 +4,24 @@ import java.util.Scanner;
 
 public class sunday {
     public static void main(String[] args) {
-        //question 1 : Throw and Catch Exception with Try-Catch
+        //question 2 : Throw and Catch Exception with Try-Catch
         Scanner sc = new Scanner(System.in);
-        System.out.println("Insert first number:");
+        System.out.println("Insert number:");
         double a = sc.nextDouble();
-        System.out.println("Insert second number:");
-        double b = sc.nextDouble();
+        tryNumber(a);
+    }
+
+    public static void tryNumber(double a){
         try{
-            double result = divideNumber(a,b);
-            System.out.println(result);
-        }catch (ArithmeticException e ){
+            checkNumber(a);
+            System.out.println(a+" is even.");
+        }catch (IllegalArgumentException e ){
             System.out.println("Error : "+e.getMessage());
         }
     }
-    public static double divideNumber(double dividend, double divisor){
-        if(divisor==0){
-            throw new ArithmeticException("Cannot divide the given number by zero!");
+    public static void checkNumber(double num){
+        if(num%2 !=0){
+            throw  new IllegalArgumentException(num + " is odd.");
         }
-        return dividend/divisor;
     }
 }
