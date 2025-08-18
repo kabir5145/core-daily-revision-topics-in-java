0be05cc3cb2dbcf2dfdedc60d6e284069a81e62b
kabir5145 <kabir10112006@gmail.com>
2025-08-18 23:26:15 +0530

    question 3 of loops_conditional

--- com/company/loops_conditional.java
+++ com/company/loops_conditional.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class loops_conditional {
     public static void main(String[] args) {
         //question 2: Find greatest among three numbers
+        /*
         Scanner sc = new Scanner(System.in);
         System.out.println("First number is : ");
         double a = sc.nextDouble();
@@ -24,5 +25,47 @@ public class loops_conditional {
         if(c>a)
             if(c>b)
                 System.out.println("Greatest number is : "+c);
+         */
+
+        //question 3: Check Positive,Negative or Zero (with Range)
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input value: ");
+        double input = in.nextDouble();
+
+        if (input > 0)
+        {
+            if (input < 1)
+            {
+                System.out.println("Positive small number");
+            }
+            else if (input > 1000000)
+            {
+                System.out.println("Positive large number");
+            }
+            else
+            {
+                System.out.println("Positive number");
+            }
+        }
+        else if (input < 0)
+        {
+            if (Math.abs(input) < 1)
+            {
+                System.out.println("Negative small number");
+            }
+            else if (Math.abs(input) > 1000000)
+            {
+                System.out.println("Negative large number");
+            }
+            else
+            {
+                System.out.println("Negative number");
+            }
+        }
+        else
+        {
+            System.out.println("Zero");
+        }
+
     }
 }
