7e4bbeba5de78d25835120898d68dc6b33c46054
kabir5145 <kabir10112006@gmail.com>
2025-08-18 23:09:06 +0530

    question 2 of loops_conditional

--- com/company/loops_conditional.java
+++ com/company/loops_conditional.java
@@ -7,6 +7,22 @@ import java.util.Scanner;
 public class loops_conditional {
     public static void main(String[] args) {
         //question 2: Find greatest among three numbers
+        Scanner sc = new Scanner(System.in);
+        System.out.println("First number is : ");
+        double a = sc.nextDouble();
+        System.out.println("Second number is : ");
+        double b = sc.nextDouble();
+        System.out.println("Third number is : ");
+        double c = sc.nextDouble();
 
+        if(a >b)
+            if(a>c)
+                System.out.println("Greatest number is : "+a);
+        if(b>a)
+            if(b>c)
+                System.out.println("Greatest number is : "+b);
+        if(c>a)
+            if(c>b)
+                System.out.println("Greatest number is : "+c);
     }
 }
