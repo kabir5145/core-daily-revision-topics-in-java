6be764c78e2adad8969247483c9ddfd9fe8cbaca
kabir5145 <kabir10112006@gmail.com>
2025-08-19 23:31:46 +0530

    question 9 of array

--- com/company/array.java
+++ com/company/array.java
@@ -1,126 +1,27 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.OptionalInt;
+import java.util.Scanner;
 
 public class array {
     public static void main(String[] args) {
-        // Question 1: Write a Java program to sort a numeric array and a string array
-        /*
-        int[] number = {1, 5, 7, 3, 4, 9, 10};
-
-        System.out.println("The original array is: " + Arrays.toString(number));
-
-        // Sort the array
-        Arrays.sort(number);
-
-        System.out.println("The sorted array is: " + Arrays.toString(number));
-
-        String[] word = {"c","b","a"};
-        System.out.println("The original array is : "+Arrays.toString(word));
-
-        //Sort the array
-        Arrays.sort(word);
-
-        System.out.println("The sorted array is :"+Arrays.toString(word));
-         */
-
-        //question 2: sum all values in an array
-        /*
-        int [] num = {12,34,45};
-        int sum = 0;
-        for(int i :num)
-            sum += i;
-        System.out.println("The sum of an array is : "+sum);
-         */
-
-        //question 3: print 10*10 grid of dashes
-/*
-        int [][] matrix = new int[10][10];
-        for(int i = 0;i< 10;i++){
-            for(int k = 0;k<10;k++) {
-                System.out.print("-");
-            }
-            System.out.println();
-        }
- */
-
-        //question 4: Calculate average of array elements
-        /*
-        int [] avg = {12,45,23};
-        int average =0;
-        for(int i = 0;i<avg.length;i++){
-            average = average + avg[i];
-        }
-        double av = average/avg.length;
-        System.out.println("The average of the numbers are : "+av );
-         */
-
-        //question 5: Check if an array contains a specific value
-        /*
+        //question 9 : Insert element at specific position
         Scanner sc = new Scanner(System.in);
-        int [] numbers = {12,34,21,22,3};
-        System.out.println("Insert a number -->");
-        int find = sc.nextInt();
-        boolean found = false;
-        for (int i = 0;i<numbers.length;i++){
-            if(numbers[i]==find){
-                found = true;
-                break;
-            }
-        }
-        if(found){
-            System.out.println("Yes ! We have found the number.");
-        }else {
-            System.out.println("Oops ! That is a wrong number.");
-        }
-         */
+        int [] a= {13,14,15,16,17};
+        System.out.println("Position of the number where you want to insert : ");
+        int insertPosition = sc.nextInt();
+        System.out.println("The new number is : ");
+        int newValue = sc.nextInt();
+        System.out.println("The new number to insert is : "+newValue);
+        System.out.println("The original array is : "+Arrays.toString(a));
 
-        //question 6: Given a 2D integer array mat[][] and a number x,
-        // find whether element x is present in the matrix or not.
-        /*
-        Scanner sc = new Scanner(System.in) ;
-        int x=0;
-        int n = 0;
-        int m = 0;
-        int  [][] mat = new int[n][m];
-        boolean found = false;
-      for(int i = 0;i<mat.length;i++){
-          for(int k = 0;k<mat[i].length;k++) {
-              if (mat[i][k] == x) {
-                  found = true;
-              }
-          }
-      }
-      found= false;
-         */
-
-        //question 7: Remove specific element from array
-        /*
-        int [] number = {12,23,21,44,54,33};
-        int remove = 1;
-        for(int i = remove;i< number.length-1;i++){
-            number[i] = number[i+1];
+        for(int i= a.length-1;i>insertPosition;i--){
+            a[i]= a[i-1];
         }
-        System.out.println("After removing the second element : "+Arrays.toString(number));
-         */
-
-        //question 8 : Copy array using iteration
-        /*
-        int [] num ={12,23,21,44,54,33};
-        int []copy = Arrays.copyOf(num,num.length);
-        for(int i = 0;i< copy.length;i++){
-            System.out.println(copy[i]);
-        }
-        System.out.println("copied array is : "+Arrays.toString(copy));
-         */
-/*
-        int[] num = {12,12,12};
-        int total = Arrays.stream(num).sum();
-        System.out.println("The sum of an array in a new way is :"+total);
- */
-//        int [] num = {1,32,65,97,54};
-//        OptionalInt m = Arrays.stream(num).max();
-//        System.out.println(m.getAsInt());
+        a[insertPosition]= newValue;
+        System.out.println("New array is : "+Arrays.toString(a));
     }
 }
