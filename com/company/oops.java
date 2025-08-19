15c20daf194a9992fefc3557db19eda9622d2571
kabir5145 <kabir10112006@gmail.com>
2025-08-20 00:25:04 +0530

    question 1 of constructor in OOPS

--- com/company/oops.java
+++ com/company/oops.java
@@ -1,65 +1,30 @@
 package com.company;
 
-/*
-class question_2{
-    public String name;
-    public int age;
-    public String getName(){
-        return name;
-    }
+import java.util.concurrent.Callable;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+class  Cat{
+    String name;
+    int age;
 
-    public int getAge() {
-        return age;
+    public Cat() {
+        this.name = "kitten";
+        this.age = 3;
     }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-}
- */
-/*
-class question_1{
-    public String name;
-    public int age;
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return age;
     }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
- */
-
 public class oops {
     public static void main(String[] args) {
-        /*
-        question_1 s = new question_1();
-        s.setAge(23);
-        s.setName("kabir");
-        System.out.println("the first name of the person is "+s.getName() +" and age is "+s.getAge());
-        s.setName("raghav");
-        System.out.println(s.getName);
-         */
-/*
-        question_2 s = new question_2();
-        s.setAge(3);
-        s.setName("booz");
-        System.out.println(s.getAge()+s.getName());
- */
+        //question 1: Default Constructor
+        Cat c = new Cat();
+        System.out.println("Cat's name : "+c.getName());
+        System.out.println("Cat's age : "+c.getAge());
         }
     }
 
