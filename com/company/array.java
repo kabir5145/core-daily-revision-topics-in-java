1008dbb25f35682600cb96d46f94fe06e0921649
kabir5145 <kabir10112006@gmail.com>
2025-08-22 22:49:02 +0530

    question 11 of array

--- com/company/array.java
+++ com/company/array.java
@@ -5,22 +5,22 @@ import java.util.*;
 
 public class array {
     public static void main(String[] args) {
-        //question 10 : Find max and min in an array
+        //question 10 : Reverse an integer array
         int[] arr = {19, 87, 66, 54, 44, 56, 65};
-        System.out.println("The original array is : " + Arrays.toString(arr));
-        int max = arr[0];
-        int min = arr[0];
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            // Swap elements
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-            if (arr[i] < min) {
-                min = arr[i];
-            }
+            start++;
+            end--;
         }
-        System.out.println("The max array is : " + max);
-        System.out.println("The min array is : " + min);
 
+        // Print reversed array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 }
