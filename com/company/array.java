8d19c1c4677ecb228f91b67afd14b2882ae40a7d
kabir5145 <kabir10112006@gmail.com>
2025-08-24 17:47:29 +0530

    question 12 of array

--- com/company/array.java
+++ com/company/array.java
@@ -5,22 +5,15 @@ import java.util.*;
 
 public class array {
     public static void main(String[] args) {
-        //question 10 : Reverse an integer array
-        int[] arr = {19, 87, 66, 54, 44, 56, 65};
-        int start = 0, end = arr.length - 1;
-        while (start < end) {
-            // Swap elements
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-
-            start++;
-            end--;
-        }
-
-        // Print reversed array
-        for (int num : arr) {
-            System.out.print(num + " ");
+        //question 12 : Find duplicate in integer array
+        int[] arr = {12, 12, 45, 67, 78, 88,0,0,45,78};
+        System.out.println("The original array is : "+Arrays.toString(arr));
+        for(int i = 0;i<arr.length-1;i++){
+            for(int k = i+1;k<arr.length;k++){
+                if((arr[i]==arr[k])){
+                    System.out.println("Duplicate Element in the array is : "+arr[k]);
+                }
+            }
         }
     }
 }
