a97e323dfd797253ad72236491de8da1f5baf47b
kabir5145 <kabir10112006@gmail.com>
2025-08-24 17:50:54 +0530

    question 13 of array

--- com/company/array.java
+++ com/company/array.java
@@ -5,13 +5,13 @@ import java.util.*;
 
 public class array {
     public static void main(String[] args) {
-        //question 12 : Find duplicate in integer array
-        int[] arr = {12, 12, 45, 67, 78, 88,0,0,45,78};
+        //question 13 : Find duplicate in String array
+        String[] arr = {"kabir","kabir","raghav","raghav","aaditya"};
         System.out.println("The original array is : "+Arrays.toString(arr));
         for(int i = 0;i<arr.length-1;i++){
             for(int k = i+1;k<arr.length;k++){
                 if((arr[i]==arr[k])){
-                    System.out.println("Duplicate Element in the array is : "+arr[k]);
+                    System.out.println("Duplicate String in the array is : "+arr[k]);
                 }
             }
         }
