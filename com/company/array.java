4694353506660d0c35be9e9fe83db69786e3e12c
kabir5145 <kabir10112006@gmail.com>
2025-08-25 22:27:47 +0530

    question 14 of array

--- com/company/array.java
+++ com/company/array.java
@@ -5,13 +5,16 @@ import java.util.*;
 
 public class array {
     public static void main(String[] args) {
-        //question 13 : Find duplicate in String array
-        String[] arr = {"kabir","kabir","raghav","raghav","aaditya"};
-        System.out.println("The original array is : "+Arrays.toString(arr));
-        for(int i = 0;i<arr.length-1;i++){
-            for(int k = i+1;k<arr.length;k++){
-                if((arr[i]==arr[k])){
-                    System.out.println("Duplicate String in the array is : "+arr[k]);
+        //question 14 : Common elements in two string arrays.
+        String []a = {"kabir","raghav","AAditya"};
+        String [] b = {"Hello","kabir","AAditya"};
+
+        System.out.println("The first string is : "+Arrays.toString(a));
+        System.out.println("The second string is : "+Arrays.toString(b));
+        for(int i = 0;i<a.length;i++){
+            for(int k = 0;k<b.length;k++){
+                if(a[i]==b[k]){
+                    System.out.println("The common element is : "+a[i]);
                 }
             }
         }
