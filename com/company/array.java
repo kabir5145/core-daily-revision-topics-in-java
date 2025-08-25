22ed5c47275f2904a2691de8bf7d82abd019f1eb
kabir5145 <kabir10112006@gmail.com>
2025-08-25 22:32:46 +0530

    question 15 of array

--- com/company/array.java
+++ com/company/array.java
@@ -5,16 +5,17 @@ import java.util.*;
 
 public class array {
     public static void main(String[] args) {
-        //question 14 : Common elements in two string arrays.
-        String []a = {"kabir","raghav","AAditya"};
-        String [] b = {"Hello","kabir","AAditya"};
+        //question 15 : Common elements in two integer arrays.
+        int[] a = {12,13,14,15,16};
+        int[] b = {21,31,41,51,61,16};
 
-        System.out.println("The first string is : "+Arrays.toString(a));
-        System.out.println("The second string is : "+Arrays.toString(b));
+        System.out.println("The first array is : "+Arrays.toString(a));
+        System.out.println("The second array is : "+Arrays.toString(b));
+
         for(int i = 0;i<a.length;i++){
             for(int k = 0;k<b.length;k++){
                 if(a[i]==b[k]){
-                    System.out.println("The common element is : "+a[i]);
+                    System.out.println("The common number in the array is : "+a[i]);
                 }
             }
         }
