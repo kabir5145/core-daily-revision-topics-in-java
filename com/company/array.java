0e7a67075d72fca6102d3b5c5abe37f4153b5f4a
kabir5145 <kabir10112006@gmail.com>
2025-08-26 23:28:01 +0530

    question 16 of array

--- com/company/array.java
+++ com/company/array.java
@@ -5,19 +5,32 @@ import java.util.*;
 
 public class array {
     public static void main(String[] args) {
-        //question 15 : Common elements in two integer arrays.
-        int[] a = {12,13,14,15,16};
-        int[] b = {21,31,41,51,61,16};
+        //question 16 : Remove duplicate from array.
+        int[] a = {12,13,14,15,16,16};
 
-        System.out.println("The first array is : "+Arrays.toString(a));
-        System.out.println("The second array is : "+Arrays.toString(b));
+        int n = a.length;
 
-        for(int i = 0;i<a.length;i++){
-            for(int k = 0;k<b.length;k++){
-                if(a[i]==b[k]){
-                    System.out.println("The common number in the array is : "+a[i]);
+        //making a new array
+        int [] temp = new int[n];
+        int newLength =0;
+
+        for(int i =0;i<n;i++){
+            boolean isDuplicate = false;
+
+            for(int k = 0;k<newLength;k++){
+                if(a[i]==temp[k]){
+                    isDuplicate = true;
+                    break;
                 }
             }
+            if(!isDuplicate){
+                temp[newLength]=a[i];
+                newLength++;
+            }
+        }
+        System.out.println("The sorted array is : ");
+        for(int i = 0;i<newLength;i++){
+            System.out.print(temp[i]+" ");
         }
     }
 }
