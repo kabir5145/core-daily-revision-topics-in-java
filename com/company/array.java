87c0ec8739ab23f09e3307d7b39c213bab1161c5
kabir5145 <kabir10112006@gmail.com>
2025-08-28 00:25:31 +0530

    question 17 of arrays

--- com/company/array.java
+++ com/company/array.java
@@ -6,17 +6,22 @@ import java.util.*;
 public class array {
     public static void main(String[] args) {
         //question 17 : Find the second-largest number.
-        int []arr= {12,13,14,15,16,67};
+        int []arr= {12,13,88,14,15,16,67};
 
         System.out.println("The array is : "+Arrays.toString(arr));
 
-        int max = arr[0];
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
 
-        for(int i = 0;i<arr.length;i++){
-            if(arr[i]>max){
-                max = arr[i-1];
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
             }
         }
-        System.out.println("The max second-element in the array is : "+max);
+
+        System.out.println("The second-largest element in the array is : " + secondLargest);
     }
 }
