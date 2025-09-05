0df5a567be24d8ed25b5c1f0ad4ba86b1d83590c
kabir5145 <kabir10112006@gmail.com>
2025-09-06 01:07:10 +0530

    question 19 of arrays adding two matrices

--- com/company/array.java
+++ com/company/array.java
@@ -5,27 +5,70 @@ import java.util.*;
 
 public class array {
     public static void main(String[] args) {
-        //question 17 : Find the second-largest number.
-        int []arr= {12,13,88,14,15,16,67};
+        //question 19 : Add two same-size matrices.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of columns you want to add in first matrix->");
+        int m = sc.nextInt();
+        System.out.println("Enter the number of rows you want to add in first matrix->");
+        int n = sc.nextInt();
+        System.out.println("Enter the number of columns you want to add in second matrix->");
+        int a = sc.nextInt();
+        System.out.println("Enter the number of rows you want to add in second matrix->");
+        int b = sc.nextInt();
+        int[][] arr1 = new int[m][n];
+        int[][] arr2 = new int[m][n];
+        int[][] sum = new int[m][n];
 
-        System.out.println("The array is : "+Arrays.toString(arr));
+        //first matrix
 
-        Arrays.sort(arr);
-
-        int index = arr.length-1;
-        while (arr[index]==arr[arr.length-1]) {
-            index--;
-        }
-        System.out.println("Second largest value : "+arr[index]);
-
-        //question 18: Find the second-smallest number.
-        Arrays.sort(arr);
-        int min = arr[0]+1;
-        for(int i =0;i<arr.length;i++){
-            if (arr[i]<min) {
-                min = arr[i+1];
+        //input
+        System.out.println("Enter the numbers for your first matrix:");
+        for (int i = 0; i < arr1.length; i++) {
+            for (int k = 0; k < arr1.length; k++) {
+                arr1[i][k] = sc.nextInt();
             }
         }
-        System.out.println("The minimum number in the array is : "+min);
+
+        //output
+        for (int i = 0; i < arr1.length; i++) {
+            for (int k = 0; k < arr1.length; k++) {
+                System.out.print(arr1[i][k] + " ");
+            }
+            System.out.println();
+        }
+
+
+        //second matrix
+        //input
+        System.out.println("Enter the numbers for your second matrix:");
+        for (int i = 0; i < arr2.length; i++) {
+            for (int k = 0; k < arr2.length; k++) {
+                arr2[i][k] = sc.nextInt();
+            }
+        }
+
+        //output
+        for (int i = 0; i < arr2.length; i++) {
+            for (int k = 0; k < arr2.length; k++) {
+                System.out.print(arr2[i][k]+" ");
+            }
+            System.out.println();
+        }
+
+        //Adding both the matrices.
+        for(int i = 0;i<sum.length;i++){
+            for(int k=0;k< sum.length;k++){
+                sum[i][k]=arr1[i][k]+arr2[i][k];
+            }
+        }
+
+        //Display the result
+        System.out.println("The result is :-> ");
+        for(int i = 0;i<sum.length;i++){
+            for(int k=0;k< sum.length;k++){
+                System.out.print(sum[i][k]+" ");
+            }
+            System.out.println();
+        }
     }
 }
