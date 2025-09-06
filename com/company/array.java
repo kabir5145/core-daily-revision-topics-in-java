6758b73053aefafd5721591c699961dcf0322cb3
kabir5145 <kabir10112006@gmail.com>
2025-09-06 22:15:18 +0530

    question 21 of arrays converting an ArrayList to an array

--- com/company/array.java
+++ com/company/array.java
@@ -5,16 +5,22 @@ import java.util.*;
 
 public class array {
     public static void main(String[] args) {
-        //question 20 : Convert array to Arraylist.
-
-        //Create an array of strings.
-        String[] s = {"Java","C++","Python","Web dev","PHP"};
-        System.out.println(Arrays.toString(s));
+        //question 21 : Convert Arraylist to array.
 
         //Create an Arraylist of strings and initialize it with the contents of the array.
-        ArrayList list = new ArrayList(Arrays.asList(s));
-
-        //print the Arraylist to the console.
+        ArrayList<String> list = new ArrayList();
+        list.add("Python");
+        list.add("Java");
+        list.add("HTML");
+        list.add("CSS");
+        list.add("Java script");
         System.out.println(list);
+
+        String []s = new String[list.size()];
+        list.toArray(s);
+
+        for(String string : s){
+            System.out.print(string+" ");
+        }
     }
 }
