6945e8e42d22ab8d16f05fdb96e156332b162f76
kabir5145 <kabir10112006@gmail.com>
2025-09-07 18:51:53 +0530

    question 1 of oops

---
+++ com/company/Object_oriented_programming.java
@@ -0,0 +1,18 @@
+package com.company;
+
+class cat{
+    String name;
+    int age;
+
+    public cat(){
+        System.out.println(this.name="kitten");
+        System.out.println(this.age=8);
+    }
+}
+public class Object_oriented_programming {
+    public static void main(String[] args) {
+        //question 1
+        cat c = new cat();
+       // c.getInfo();
+    }
+}
