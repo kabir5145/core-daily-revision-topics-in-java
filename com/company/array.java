f3036cd9dfe26fa314f7f8489fb4d48ef2e56568
kabir5145 <kabir10112006@gmail.com>
2025-09-11 15:17:19 +0530

    question 23 of arrays in java

--- com/company/array.java
+++ com/company/array.java
@@ -5,21 +5,27 @@ import java.util.*;
 
 public class array {
     public static void main(String[] args) {
-        //question 22 : Find the pairs with a given sum.
+        //question 23 : Check if two arrays are equal.
 
-        int []arr = {1,2,3,4,5,6,7,8};
-        Scanner sc = new Scanner(System.in);
+        int[] arr_1 = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] arr_2 = {1, 2, 3, 4, 5, 6, 7, 8};
 
-        int sum = sc.nextInt();
+        boolean isEqual = true;
 
-        System.out.println("Pairs with sum " + sum + " are:");
-
-        for(int i = 0;i<arr.length;i++){
-            for(int k = 0;k<arr.length;k++){
-                if(arr[i]+arr[k] == sum){
-                    System.out.println("(" + arr[i] + ", " + arr[k] + ")");
+        if(arr_1.length != arr_2.length) {
+            isEqual = false;
+        }else {
+            for(int i = 0;i<arr_1.length;i++){
+                if(arr_1[i] != arr_2[i]){
+                    isEqual = false;
+                    break;
                 }
             }
+        }
+        if(isEqual){
+            System.out.println("Both the arrays are equal !");
+        }else {
+            System.out.println("Two arrays are not equal !");
         }
     }
 }
