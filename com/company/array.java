21cb70a9717afe3694c7f662ce15c30ee3f0ec8f
kabir5145 <kabir10112006@gmail.com>
2025-09-12 18:48:16 +0530

    question 26 of arrays in java.

--- com/company/array.java
+++ com/company/array.java
@@ -5,33 +5,19 @@ import java.util.*;
 
 public class array {
     public static void main(String[] args) {
-        //Question 25 : Common elements in three sorted arrays.
-        int [] arr_1 = {32,12,34,31,11};
-        Arrays.sort(arr_1);
-        int [] arr_2 = {11,12,54,98,76,67};
-        Arrays.sort(arr_2);
-        int [] arr_3 = {100,12,11,364,383};
-        Arrays.sort(arr_3);
+     //Question 26: Move all zeros to array end.
+     int [] arr = {12,1,2,0,0,0,0,3,2,56,32};
+     int c = 0;
 
-        int i = 0;
-        int j = 0;
-        int k = 0;
-
-        System.out.print("Common elements: ");
-
-        while (i < arr_1.length && j < arr_2.length && k < arr_3.length) {
-            if (arr_1[i] == arr_2[j] && arr_2[j] == arr_3[k]) {
-                System.out.print(arr_1[i] + " ");
-                i++;
-                j++;
-                k++;
-            } else if (arr_1[i] < arr_2[j]) {
-                i++;
-            } else if (arr_2[j] < arr_3[k]) {
-                j++;
-            } else {
-                k++;
-            }
-        }
+     for(int i = 0;i<arr.length;i++){
+         if(arr[i]!=0){
+             arr[c++]= arr[i];
+         }
+     }
+     while (c<arr.length){
+         arr[c++]=0;
+     }
+     for(int num : arr)
+         System.out.print( num+" ");
     }
 }
