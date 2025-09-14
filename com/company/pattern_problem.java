8f3a5c1ddd1b7f950432b27c2a620939ced864c4
kabir5145 <kabir10112006@gmail.com>
2025-09-14 15:57:40 +0530

    question 2 of making a pattern in java.

--- com/company/pattern_problem.java
+++ com/company/pattern_problem.java
@@ -2,13 +2,17 @@ package com.company;
 
 public class pattern_problem {
     public static void main(String[] args) {
-    //Question 1
+    //Question 2
         int a = 5;
         int b = 5;
 
         for(int i =0;i<a;i++){
-            for(int k = 0;k<b;k++){
-                System.out.print("*"+" ");
+            for(int k = 0;k<b;k++) {
+                if(i==0 || k==0 || i == a-1 || k ==b-1){
+                    System.out.print("*"+" ");
+                }else {
+                    System.out.print(" "+" ");
+                }
             }
             System.out.println();
         }
