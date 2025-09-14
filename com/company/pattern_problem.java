daa78ea32c67b5a482698c1316ece7ac1bf84255
kabir5145 <kabir10112006@gmail.com>
2025-09-14 15:48:55 +0530

    question 1 of making a pattern in java.

---
+++ com/company/pattern_problem.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class pattern_problem {
+    public static void main(String[] args) {
+    //Question 1
+        int a = 5;
+        int b = 5;
+
+        for(int i =0;i<a;i++){
+            for(int k = 0;k<b;k++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
