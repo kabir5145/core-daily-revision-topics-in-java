28b26ba2683fa103bfc9ddebfc1e45674de0b7f2
kabir5145 <kabir10112006@gmail.com>
2025-09-16 17:22:38 +0530

    question 10 of strings in java

--- com/company/string_question.java
+++ com/company/string_question.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 
 public class string_question {
     public static void main(String[] args) {
-        //Question 9 : Compare with Char Sequence.
+        //Question 10 : Compare with StringBuffer.
         String str_1 = "hello how are you ",str_2 = "Hello How Are You";
 
-        CharSequence cs = "hello how are you ";
+        StringBuilder cs = new StringBuilder("hello how are you ");
 
         System.out.println(str_1.contentEquals(cs));
         System.out.println(str_2.contentEquals(cs));
