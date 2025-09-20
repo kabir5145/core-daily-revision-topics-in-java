9ac3350b77e8a6e0735602959b3e0cbf4fbb2f73
kabir5145 <kabir10112006@gmail.com>
2025-09-20 22:14:20 +0530

    question 12 of Strings in java

--- com/company/string_question.java
+++ com/company/string_question.java
@@ -1,15 +1,12 @@
 package com.company;
 
-import java.awt.*;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class string_question {
     public static void main(String[] args) {
-        //Question 11: String from Character Array.
-        char []arr_num = new char[]{'1','2','3','4'};
-
-        String str = String.copyValueOf(arr_num,1,3);
-
-        System.out.println("The book contains "+str+" pages.");
+        //Question 12: Print Current Date and Time.
+        System.out.println("Current Date : "+ LocalDate.now());
+        System.out.println("Current Time : "+ LocalTime.now());
     }
 }
