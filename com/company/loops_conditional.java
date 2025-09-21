a81a6f1a02ddef53f3ddc89f5f5cfc98aacbf074
kabir5145 <kabir10112006@gmail.com>
2025-09-21 21:44:35 +0530

    question 7 of loops_conditional in java

--- com/company/loops_conditional.java
+++ com/company/loops_conditional.java
@@ -6,13 +6,17 @@ import java.util.Scanner;
 
 public class loops_conditional {
     public static void main(String[] args) {
-       //Question 6: Display First 10 Natural Numbers.
+       //Question 7: Sum of N natural number.
 
         System.out.println("First 10 natural no. are : ");
 
+        int n = 0;
         for(int i = 1;i<=10;i++){
-            System.out.print(i+" ");
+            System.out.println(i+" ");
+
+            n +=i;
+
         }
-        System.out.println();
+        System.out.println("The sum is : "+n);
     }
 }
