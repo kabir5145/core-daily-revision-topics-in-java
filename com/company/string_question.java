cc22f8ab155381a21bb2f02aa02a1afd5315f10c
kabir5145 <kabir10112006@gmail.com>
2025-09-23 22:22:54 +0530

    question 13 of string in java

--- com/company/string_question.java
+++ com/company/string_question.java
@@ -2,11 +2,14 @@ package com.company;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Scanner;
 
 public class string_question {
     public static void main(String[] args) {
-        //Question 12: Print Current Date and Time.
-        System.out.println("Current Date : "+ LocalDate.now());
-        System.out.println("Current Time : "+ LocalTime.now());
+        //Question 13: String Length.
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println("The original string is : "+s);
+        System.out.println("The string length is : "+s.length());
     }
 }
