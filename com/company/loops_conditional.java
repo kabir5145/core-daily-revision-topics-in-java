2669a66c22dda00688572b8775bb16e08913806e
kabir5145 <kabir10112006@gmail.com>
2025-09-24 20:47:31 +0530

    question 8 of loops_conditional in java

--- com/company/loops_conditional.java
+++ com/company/loops_conditional.java
@@ -6,12 +6,17 @@ import java.util.Scanner;
 
 public class loops_conditional {
     public static void main(String[] args) {
-       //Question 8: Average of three number.
+        //Question 9 : Display Cubes of Numbers.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        int avg = (a+b+c)/3;
-        System.out.println(avg);
+        int cube_1 = a*a*a;
+        int cube_2 = b*b*b;
+        int cube_3 = c*c*c;
+
+        System.out.println("Cube of  a is : "+cube_1);
+        System.out.println("Cube of  b is : "+cube_2);
+        System.out.println("Cube of  c is : "+cube_3);
     }
 }
