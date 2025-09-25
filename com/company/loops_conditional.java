0d9010c7be3a06f871f0247e5943b0e0cbb62ca8
kabir5145 <kabir10112006@gmail.com>
2025-09-25 23:05:01 +0530

    question 10 of loops_conditional in java

--- com/company/loops_conditional.java
+++ com/company/loops_conditional.java
@@ -6,17 +6,13 @@ import java.util.Scanner;
 
 public class loops_conditional {
     public static void main(String[] args) {
-        //Question 9 : Display Cubes of Numbers.
+        //Question 10 : Multiplication Table of a number
+        int a = 10;
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
         int b = sc.nextInt();
-        int c = sc.nextInt();
-        int cube_1 = a*a*a;
-        int cube_2 = b*b*b;
-        int cube_3 = c*c*c;
-
-        System.out.println("Cube of  a is : "+cube_1);
-        System.out.println("Cube of  b is : "+cube_2);
-        System.out.println("Cube of  c is : "+cube_3);
+        System.out.println("Table to be calculated : "+b);
+       for(int i =1;i<=a;i++){
+           System.out.println(i+" * "+b+" = "+i*2);
+       }
     }
 }
