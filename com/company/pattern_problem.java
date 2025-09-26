c6df03dcee392d2dbf35d902cb8603fa34d89cfb
kabir5145 <kabir10112006@gmail.com>
2025-09-26 20:21:51 +0530

    question 4 of pattern problem in java

--- com/company/pattern_problem.java
+++ com/company/pattern_problem.java
@@ -2,16 +2,13 @@ package com.company;
 
 public class pattern_problem {
     public static void main(String[] args) {
-        //Question 3
+        //Question 4
         int a = 5;
         int b = 5;
 
         for (int i = 0; i < a; i++) {
-            for (int k = 0; k < b-i; k++) {
-                System.out.print(" ");
-            }
-            for(int j = 0;j<=i;j++){
-                System.out.print(i+" ");
+            for (int k = 0; k < b - i; k++) {
+                System.out.print("*");
             }
             System.out.println();
         }
