b2cf406849c39cb810d02a6cd7992e675e32c865
kabir5145 <kabir10112006@gmail.com>
2025-09-27 22:36:38 +0530

    question 31 of arrays in java

--- com/company/array.java
+++ com/company/array.java
@@ -5,23 +5,13 @@ import java.util.*;
 
 public class array {
     public static void main(String[] args) {
-        //Question 30: Check if array excludes 0 and -1.
+        //Question 31: Sum of an array .
         int []arr = {13,33,21,45};
 
-        System.out.println("The array is : "+Arrays.toString(arr));
-
-        boolean excludes = true;
-
-        for(int num : arr){
-            if(num==0 || num==-1){
-                excludes = false;
-                break;
-            }
+        int sum = 0;
+        for(int i = 0;i<arr.length;i++){
+            sum +=arr[i];
         }
-        if(excludes){
-            System.out.println("The array excludes from 0 to -1.");
-        }else {
-            System.out.println("The array not excludes from 0 to -1.");
-        }
+        System.out.println(sum);
     }
 }
